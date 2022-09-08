4c577126a742a270bb1a58d194ed2060c3add811
ghost <2127840450@qq.com>
2022-09-08 10:45:23 +0800

    迭代实现

--- src/Tree/L101.java
+++ src/Tree/L101.java
@@ -1,5 +1,6 @@
 package Tree;
 
+import java.awt.desktop.QuitEvent;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -14,19 +15,46 @@ public class L101 {
 }
 
 class Solution101 {
+    //递归实现
     public boolean isSymmetric(TreeNode root) {
         if (root == null)
             return true;
-        return judge(root.left,root.right);
+        return judge(root.left, root.right);
     }
-    public boolean judge(TreeNode a, TreeNode b){
-        if(a == null && b == null){
+
+    public boolean judge(TreeNode a, TreeNode b) {
+        if (a == null && b == null) {
             return true;
-        }else if(a ==null || b ==null){
+        } else if (a == null || b == null) {
             return false;
-        }else if(a.val != b.val){
+        } else if (a.val != b.val) {
             return false;
         }
-        return judge(a.left,b.right) && judge(a.right,b.left);
+        return judge(a.left, b.right) && judge(a.right, b.left);
+    }
+
+    //迭代实现
+    public boolean isSymmetric2(TreeNode root) {
+        if (root == null)
+            return true;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root.left);
+        queue.offer(root.right);
+        while (!queue.isEmpty()) {
+            TreeNode a = queue.poll();
+            TreeNode b = queue.poll();
+            if (a != null && b != null) {
+                if (a.val != b.val) {
+                    return false;
+                }
+                queue.offer(a.left);
+                queue.offer(b.right);
+                queue.offer(a.right);
+                queue.offer(b.left);
+            } else if (a == null && b != null || a != null && b == null) {
+                return false;
+            }
+        }
+        return true;
     }
 }
